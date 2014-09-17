1af3f7acdbe868c0eda39ab26e2feb6a23fde6fa
jiamingkong <kongjiaming@gmail.com>
2014-09-17 14:50:47 -0400

    Starting on Leetcode

---
+++ plus_one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+    	// Only one thing to worry. What if the result is one digit longer than the given number?
+      int carry = 1;
+      int current_digit = 0;
+      int n = digits.size();
+
+      int BASE = 10;
+      vector<int> result(n, 0);
+      int i = 0;
+      for(i = n - 1; i >= 0; i--)
+      {
+        current_digit = digits[i] + carry;
+        carry = current_digit / BASE;
+        result[i] = current_digit % BASE;
+      }
+      if(carry == 1)
+      {
+        result.insert(result.begin(), 1);
+      }
+      return result;
+    }
+};
+
---
+++ same_tree.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+  		if(p==NULL && q==NULL) return true; // reaching the end of the tree
+  		if(!p || !q) return false; // after catching the 'end' case, what's left should be pretty much not symmetry.
+  		return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
