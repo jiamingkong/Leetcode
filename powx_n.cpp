fd2cbb49320ffe681f979bbed7b70c467eea5393
jiamingkong <kongjiaming@gmail.com>
2014-09-24 17:06:01 -0400

    Day 4, did 2 more easy ?s

--- powx_n.cpp
+++ powx_n.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     double pow(double x, int n) {
+        /*This problem is relatively simple, the n here must be integer which helps a lot.
+        Say I want X^397, instead of doing result *= x for 397 times, a faster way works like :
+        x^397 = x^198 * x^198 * x
+              = (x^98 * x^98) ^ 2 * x
+              = ....
+              */
+
+
         if (n < 0)
             return 1.0 / power(x , -n);
         else
