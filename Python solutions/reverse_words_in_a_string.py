ac7de339c31ec6bee18619163c2fcc7ad6e6709b
Jiaming Kong <kongjiaming@gmail.com>
2014-12-30 12:49:24 -0500

    Day 13

--- Python solutions/reverse_words_in_a_string.py
+++ Python solutions/reverse_words_in_a_string.py
@@ -1,12 +1,6 @@
 class Solution:
     # @param s, a string
     # @return a string
-
     def reverseWords(self, s):
         words = s.split(" ")
         return " ".join([i for i in words[::-1] if i != ""])
-
-
-# if __name__ == '__main__':
-# 	print Solution().reverseWords("The sky is blue")
-# 	print Solution().reverseWords(" ")
